a04bf8395a9e57c738503ccb8b74e28252c98d0f
Antisocial <aviewfromthe6ix@gmail.com>
2026-09-21 19:51:48 +0100

    updated.env

--- api-config.js
+++ api-config.js
@@ -1,4 +1,5 @@
 // IMPORTANT: swap this to the real, deployed Vercel URL for APE-P-I
 // once both this app and the API are deployed. Currently pointed at
 // local dev for testing.
-export const API_BASE_URL = 'http://localhost:3001';
+export const API_BASE_URL = https://ape-p-l7t184xzd-antisocial-s-projects.vercel.app
+
